c1dad6177b4bdf56ac2340ff6a9fa058dbc65075
Nitish Bhasker <nkbhasker@gmail.com>
2020-09-27 13:33:34 +0530

    change const file name

---
+++ src/const/profile/profile-message.const.ts
@@ -0,0 +1,9 @@
+export const INVESTOR_PROFILE_EXISTS = 'Investor profile exists';
+
+export const ADVISOR_PROFILE_EXISTS = 'Advisor profile exists';
+
+
+export const INVESTOR_CREATE_SUCCESS = 'Investor profile created successfully';
+
+
+export const ADVISOR_CREATE_SUCCESS = 'Advisor profile created successfully';
